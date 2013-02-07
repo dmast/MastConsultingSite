bc2876ba67713ac7902cf1d4593e9b56067b8ab5
David Mast <dave.mast@gmail.com>
2013-02-06 20:33:45 -0500

    added static assets for bootstrap, updated site with main page info

--- MastConsulting/settings.py
+++ MastConsulting/settings.py
@@ -38,6 +38,15 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
+    )
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ''
@@ -51,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -62,6 +71,7 @@ STATICFILES_DIRS = (
 # Put strings here, like "/home/html/static" or "C:/www/django/static".
 # Always use forward slashes, even on Windows.
 # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, "static/img"),
 )
 
 # List of finder classes that know how to find static files in
@@ -120,6 +130,7 @@ INSTALLED_APPS = (
     #'django.contrib.admindocs',
     'polls',
     'bootstrap_toolkit',
+    'MastConsulting',
     )
 
 # A sample logging configuration. The only tangible logging
