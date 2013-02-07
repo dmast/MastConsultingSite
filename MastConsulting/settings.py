8e8047aeb6339d1649aa524a9ce48c1fcb311deb
David Mast <dave.mast@gmail.com>
2013-02-06 21:46:11 -0500

    added new fonts and layout of main page

--- MastConsulting/settings.py
+++ MastConsulting/settings.py
@@ -72,6 +72,8 @@ STATICFILES_DIRS = (
 # Always use forward slashes, even on Windows.
 # Don't forget to use absolute paths, not relative paths.
     os.path.join(PROJECT_DIR, "static/img"),
+    os.path.join(PROJECT_DIR, "static/css"),
+    os.path.join(PROJECT_DIR, "static/font"),
 )
 
 # List of finder classes that know how to find static files in
